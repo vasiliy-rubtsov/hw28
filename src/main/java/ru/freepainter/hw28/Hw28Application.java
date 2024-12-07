857d9e64b380cf58df5ae273577e7a5251927fff
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2024-12-07 15:03:18 +0300

    Initial Commit

---
+++ src/main/java/ru/freepainter/hw28/Hw28Application.java
@@ -0,0 +1,13 @@
+package ru.freepainter.hw28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw28Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/freepainter/hw28/Hw28ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.freepainter.hw28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
